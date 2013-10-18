2341430eaab468ce25547adfb40b384f54e0ff4b
Lars Kotthoff <larsko@4c.ucc.ie>
2013-10-18 16:19:22 +0100

    line-by-line output and check whether machine is running

--- static/user.js
+++ static/user.js
@@ -1,13 +1,6 @@
 var control = document.getElementById('control'),
     button = document.getElementById('open');
 
-function mkDiv(content) {
-  el = document.createElement("div");
-  el.className = "output";
-  el.innerHTML = content.replace(/\n/g, "<br/>");
-  document.body.appendChild(el);
-}
-
 function controlVM() {
     var parts = document.location.pathname.split('/')
       , base = parts.slice(0, parts.length - 1).join('/') + '/'
@@ -16,27 +9,60 @@ function controlVM() {
 
     control.disabled = true;
 
+    var el = document.createElement("div");
+    el.className = "output";
+    document.body.appendChild(el);
+
     if(button.disabled) {
-      socket.emit("start", function(error, res) {
+      socket.emit("start", function(error) {
         if(error) {
-          res = error;
+          el.innerHTML = el.innerHTML + error.replace(/\n/g, "<br/>");
         } else {
           control.innerHTML = "Stop VM";
           button.disabled = false;
         }
         control.disabled = false;
-        mkDiv(res);
+        socket.removeAllListeners("output");
       });
     } else {
-      socket.emit("stop", function(error, res) {
+      socket.emit("stop", function(error) {
         if(error) {
-          res = error;
+          el.innerHTML = el.innerHTML + error.replace(/\n/g, "<br/>");
         } else {
           control.innerHTML = "Start VM";
           button.disabled = true;
         }
         control.disabled = false;
-        mkDiv(res);
+        socket.removeAllListeners("output");
       });
     }
+
+    socket.on("output", function(data) {
+      el.innerHTML = el.innerHTML + data.replace(/\n/g, "<br/>");
+    });
+}
+
+function statusVM() {
+    var parts = document.location.pathname.split('/')
+      , base = parts.slice(0, parts.length - 1).join('/') + '/'
+      , resource = base.substring(1) + 'socket.io';
+    socket = io.connect(null, { resource: resource });
+
+    control.disabled = true;
+
+    socket.emit("status", function(error) {
+      if(error) console.log(error);
+      socket.removeAllListeners("output");
+      control.disabled = false;
+    })
+
+    socket.on("output", function(data) {
+      if(data.match("running")) {
+          control.innerHTML = "Stop VM";
+          button.disabled = false;
+      } else if(data.match("poweroff")) {
+          control.innerHTML = "Start VM";
+          button.disabled = true;
+      }
+    });
 }
